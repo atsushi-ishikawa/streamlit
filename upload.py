86e47cb42333b90a79a44bf6491360d5ed30a9eb
ISHIKAWA Atsushi <ishikawa.atsushi@nims.go.jp>
2024-02-19 11:55:54 +0900

    updated

--- upload.py
+++ upload.py
@@ -13,15 +13,39 @@ else:
     df_all = pd.DataFrame()
 
 uploaded_file = st.sidebar.file_uploader("Choose a csv file")
+# get material name
 material_name = st.sidebar.text_input("Material name", placeholder="Material name")
+
+# get activity value
+activity = st.sidebar.number_input("Activity", value=0.0)
+
+# register button
 register = st.sidebar.button("Register")
+
+# show title message
+st.title("Spectrum Viewer")
+
+# show current time
+st.write("Current time: ", pd.Timestamp.now())
+
+# show uploaded file
+if uploaded_file:
+    st.write("Uploaded file: ", uploaded_file.name)
+
+# show current directory
+st.write("Current directory: ", os.getcwd())
 
 if register:
     df = pd.read_csv(uploaded_file, header=header)
-    new_df = pd.DataFrame({"material": material_name, "filename": uploaded_file.name, "y": [df[df.columns[1]].to_numpy()]})
+    new_df = pd.DataFrame({"material": material_name, "filename": uploaded_file.name,
+                           "y": [df[df.columns[1]].to_numpy()],
+                           "activity": activity
+                           })
     df_all = pd.concat([df_all, new_df])
 
-    st.dataframe(df_all, column_config={"y": st.column_config.LineChartColumn("spectrum", width="large", y_min=0, y_max=100)})
-
-    df_all.to_pickle("all.pkl")
-
+    st.dataframe(df_all,
+            column_config={
+            "y": st.column_config.LineChartColumn("spectrum", width="large", y_min=0, y_max=100),
+            "activity": st.column_config.NumberColumn("activity"),
+            }
+    )
